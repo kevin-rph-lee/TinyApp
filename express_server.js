1326288e4867da8808920d70232286b9a1ec03e8
kevin <kevin.rph.lee@gmail.com>
2017-11-10 00:19:40 +0000

    cleaned up some lint issues

--- express_server.js
+++ express_server.js
@@ -1,23 +1,19 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 8080; // default port 8080
+const PORT = process.env.PORT || 8080;
 const cookieSession = require('cookie-session');
-
 const bodyParser = require("body-parser");
 const bcrypt = require('bcrypt');
-const password = "purple-monkey-dinosaur"; // you will probably this from req.params
+const password = "purple-monkey-dinosaur";
 const hashedPassword = bcrypt.hashSync(password, 10);
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieSession({
   name: 'session',
   keys: ['key1', 'key2'],
-
   // Cookie Options
-  maxAge: 24 * 60 * 60 * 1000 // 24 hours
-}))
-
+  maxAge: 24 * 60 * 60 * 1000
+}));
 app.set("view engine", "ejs");
-
 
 //Database of URLS in system
 const urlDatabase = {
@@ -28,18 +24,16 @@ const urlDatabase = {
     date: '4-12-2011',
     visitors: []
   },
-
   "9sm5xK": {
     longURL: "http://google.ca",
     owner: '123',
     visited: 0,
-    date: '1-3-2043',
+    date: '1-3-2016',
     visitors: []
   }
-
 };
 
-//Database of Users in teh system
+//Database of Users in the system
 const users = {
   "123": {
     id: "123",
@@ -58,9 +52,9 @@ const users = {
  * @return {string} random string 6 characters long
  */
 function generateRandomString() {
-  var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  for (var i = 0; i <= 5; i++){
+  let text = "";
+  const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i <= 5; i++){
     text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
   return text;
@@ -73,12 +67,12 @@ function generateRandomString() {
  */
 function urlsForUser(id){
   let urlDatabaseForUser = {};
-  for(var url in urlDatabase){
+  for(let url in urlDatabase){
     if (urlDatabase[url].owner === id){
       urlDatabaseForUser[url] = urlDatabase[url];
     }
   }
-  return urlDatabaseForUser
+  return urlDatabaseForUser;
 }
 
 /**
@@ -88,12 +82,22 @@ function urlsForUser(id){
  * @return {boolean}       Returns true if user already exists, false otherwise
  */
 function userExists(email, users){
-  for (var user in users){
+  for (let user in users){
     if (email === users[user].email){
       return true;
     }
   }
-  return dup;
+  return false;
+}
+
+/**
+ * Given an array of visitors, checks if the userid exists within it
+ * @param  {array} visitors array of visitors within a URL
+ * @param  {string} userid   userid of user
+ * @return {boolean}          returns true if the user already exists within the array, false if not
+ */
+function checkNewVisistor(visitors, userid){
+  return visitors.indexOf(userid) === -1;
 }
 
 app.get("/", (req, res) => {
@@ -104,16 +108,18 @@ app.get("/", (req, res) => {
   }
 });
 
-
+//Main page which shows all of the short URL's that the user owns
 app.get("/urls", (req, res) => {
-  var id = req.session.user_id;
+  console.log(users);
+  const { user_ID } = req.session;
   //to debug urlDatabase: console.log(urlDatabase);
   res.render("urls_index", {
-    urls: urlsForUser(req.session.user_id),
-    user: users[req.session.user_id]
+    urls: urlsForUser(user_ID),
+    user: users[user_ID]
   });
 });
 
+//If logged in, shows the create new shortURL page
 app.get("/urls/new", (req, res) => {
   if(!req.session.user_id){
     res.redirect('/login');
@@ -126,6 +132,7 @@ app.get("/urls/new", (req, res) => {
 
 //Opens login page if not already logged in
 app.get("/login", (req, res) => {
+  const { user_ID } = req.session;
   if(!req.session.user_id){
     res.render("urls_login", {
       user: users[req.session.user_id]
@@ -138,60 +145,70 @@ app.get("/login", (req, res) => {
 //Page to update the long URL
 app.get("/urls/:id", (req, res) => {
   //sends errors if page doesn't exist or if the user is not the owner of the page
-  if(urlDatabase[req.params.id] === undefined){
+  if(!urlDatabase[req.params.id]){
     res.sendStatus(404);
-  } else if(urlDatabase[req.params.id].owner !== req.session.user_id || req.session.user_id === undefined){
+  } else if((!req.session.user_id) || (urlDatabase[req.params.id].owner !== req.session.user_id)){
     res.sendStatus(403);
   } else {
     res.render("urls_show", {
-     urls: urlDatabase,
-     shortURL: req.params.id,
-     user: users[req.session.user_id]
-   });
+      urls: urlDatabase,
+      shortURL: req.params.id,
+      user: users[req.session.user_id]
+    });
   }
 });
 
+//Registration page
 app.get("/register", (req, res) => {
   res.render('urls_register');
 });
 
+//Will redirect to the long URL
 app.get("/u/:shortURL", (req, res) => {
-  var shortURL = req.params.shortURL;
-  if(urlDatabase[shortURL] === undefined){
-    res.status(404).send('Not found!');
+  const shortURL = req.params.shortURL;
+  //Check to see if the short URL exists in the DB
+  if(!urlDatabase[shortURL]){
+    res.status(404);
   } else {
+    //increments the visitor counter and if the visistor is unique will add the visitor within the url object
     urlDatabase[shortURL].visited ++ ;
-    if(urlDatabase[shortURL].visitors.indexOf(req.session.user_id) === -1){
+    if(checkNewVisistor(urlDatabase[shortURL].visitors, req.session.user_id)){
       console.log('new visitor!');
       urlDatabase[shortURL].visitors.push(req.session.user_id);
     }
-  res.redirect(urlDatabase[shortURL].longURL);
+    res.redirect(urlDatabase[shortURL].longURL);
   }
 });
 
+//Registers a new user into the system
 app.post("/register", (req, res) => {
-  var userID = generateRandomString();
-  var email = req.body.email.toLowerCase();
-  if (req.body.email.length === 0 || req.body.password === 0){
+  const userID = generateRandomString();
+  const email = req.body.email.toLowerCase().trim();
+  //Checking to ensure the email and password are not empty
+  if (req.body.email.length === 0 || req.body.password.length === 0){
     res.sendStatus(400);
+    //Checks if user exists
   } else if(userExists(email, users)){
     res.sendStatus(400);
   } else {
-    users[userID] = {
-     id: userID,
-     email: email,
-     password: bcrypt.hashSync(req.body.password, 10)
-    }
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
+      users[userID] = {
+        id: userID,
+        email,
+        password: hash
+      };
     req.session.user_id = userID;
     res.redirect('/urls');
+    });
   }
 });
 
+//Logs the user into the system
 app.post("/login", (req, res) => {
-  for (var user in users){
-    console.log('Checking: ' + user + ' value ' + users[user].email);
-    console.log('input: ' + req.body.email);
-    if (req.body.email.toLowerCase() === users[user].email){
+  for (let user in users){
+    //Checking if the user exists
+    if (req.body.email.toLowerCase().trim() === users[user].email){
+      //Checking if password matches what we have in the DB
       if (bcrypt.compareSync(req.body.password, users[user].password)){
         req.session.user_id = users[user].id;
         res.redirect('/urls');
@@ -203,19 +220,24 @@ app.post("/login", (req, res) => {
   res.sendStatus(403);
 });
 
+//Logs out by knocking out the cookie session
 app.post("/logout", (req, res) => {
   req.session = null;
   res.redirect('/urls');
 });
 
+//Updates the longURL to the new one provided by the user
 app.post("/urls/:id", (req, res) => {
   urlDatabase[req.params.id].longURL = req.body.updateURL;
   res.redirect('/urls/');
 });
 
+//Generates a new short URL in the URL database
 app.post("/urls", (req, res) => {
-  var ran = generateRandomString();
-  var date = new Date();
+  //creating the random ID string
+  const ran = generateRandomString();
+  //Generating the created date of the shortURL
+  const date = new Date();
   urlDatabase[ran] = {
     id: ran,
     longURL: req.body['longURL'],
@@ -224,10 +246,10 @@ app.post("/urls", (req, res) => {
     date: date.getDate() + '-' + date.getMonth() + '-' + date.getFullYear(),
     visitors: []
   };
-  console.log(urlDatabase);
   res.redirect('/urls/');
 });
 
+//Deletes the shortURL from the DB
 app.post("/urls/:id/delete", (req, res) => {
   console.log(req.params.id);
   delete urlDatabase[req.params.id];
